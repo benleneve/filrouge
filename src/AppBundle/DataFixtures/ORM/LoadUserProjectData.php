29f8492cb00f1c9e2c3c805eb63ffa0e07617e0f
benjamin.dl10 <benjamin.dl10@PC-NA-20.formation.imie>
2015-02-23 14:21:10 +0100

    Premier envoi

--- src/AppBundle/DataFixtures/ORM/LoadUserProjectData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserProjectData.php
@@ -15,13 +15,48 @@ class LoadNotificationData extends AbstractFixture implements OrderedFixtureInte
     public function load(ObjectManager $manager) {
        
         $userProject1 = new UserProject();
-        $userProject1->setProject($this->getReference('project'))
-                    ->setUser($this->getReference('user'))
-                    ->setSkill();
+        $userProject1->setProject($this->getReference('project1'))
+                    ->setUser($this->getReference('user2'))
+                    ->setSkill('PHP');
         $manager->persist($userProject1);
         
+        $userProject2 = new UserProject();
+        $userProject2->setProject($this->getReference('project1'))
+                    ->setUser($this->getReference('user3'))
+                    ->setSkill('Photoshop');
+        $manager->persist($userProject2);
+        
+        $userProject3 = new UserProject();
+        $userProject3->setProject($this->getReference('project3'))
+                    ->setUser($this->getReference('user9'))
+                    ->setSkill('JavaScript');
+        $manager->persist($userProject3);
+        
+        $userProject4 = new UserProject();
+        $userProject4->setProject($this->getReference('project6'))
+                    ->setUser($this->getReference('user5'))
+                    ->setSkill('NodeJS');
+        $manager->persist($userProject1);
+        
+        $userProject5 = new UserProject();
+        $userProject5->setProject($this->getReference('project6'))
+                    ->setUser($this->getReference('user3'))
+                    ->setSkill('Photoshop');
+        $manager->persist($userProject1);
+        
+        $userProject6 = new UserProject();
+        $userProject6->setProject($this->getReference('project6'))
+                    ->setUser($this->getReference('user6'))
+                    ->setSkill('Html5 / CSS3');
+        $manager->persist($userProject6);
+        
+        $userProject7 = new UserProject();
+        $userProject7->setProject($this->getReference('project4'))
+                    ->setUser($this->getReference('user8'))
+                    ->setSkill('PHP');
+        $manager->persist($userProject7);
+        
         $manager->flush(); 
-   
     }
     
     public function getOrder() {
