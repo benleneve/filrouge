35be398c72a7c6cefee8ffb92f6c7b39b311963c
benjamin.dl10 <benjamin.dl10@PC-NA-20.formation.imie>
2015-02-23 12:18:16 +0100

    Premier envoi

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserProjectData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\UserProject;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadNotificationData extends AbstractFixture implements OrderedFixtureInterface {
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager) {
+       
+        $userProject1 = new UserProject();
+        $userProject1->setProject($this->getReference('project'))
+                    ->setUser($this->getReference('user'))
+                    ->setSkill();
+        $manager->persist($userProject1);
+        
+        $manager->flush(); 
+   
+    }
+    
+    public function getOrder() {
+        return 12;
+    }
+    
+}
