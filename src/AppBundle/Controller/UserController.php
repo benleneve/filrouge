6575df59992183d26fcff88d61f7b9466cb52bd8
sofia.dl10 <sofia.dl10@PC-NA-29.formation.imie>
2015-02-24 09:55:55 +0100

    Commit Sofia

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,25 +2,71 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller {
     
-    public function listAction(Request $req) {
-        return new Response('Page de liste utilisateur');
+    public function listAction(Request $req) 
+        {
+             $repo = $this->getDoctrine()
+                          ->getManager()
+                          ->getRepository('AppBundle:User');
+           
+             $user = $repo->findAllUsersEager();
+             
+             return $this->render('AppBundle:User:allusers.html.twig',
+                                       array(
+                                        'users' => $user
+                                        ));
     }
     
     public function detailAction($id, Request $req) {
-        return new Response('utilisateur' . $id);
+            $prod = $this->getDoctrine()
+                         ->getRepository('AppBundle:User')
+                         ->findOneUserEager($id);
+            
+            return $this->render('AppBundle:User:userDetail.html.twig',
+                                       array(
+                                        'user' => $user
+                                        ));
     }
     
-    public function updateAction($id, Request $req) {
-        return new Response('Update utilisateur' . $id);
-    }
+    public function updateAction($id, Request $req) 
+        {
+            $prod = $this->getDoctrine()
+                         ->getRepository('AppBundle:User')
+                         ->findOneUserEager($id);
+
+
+            $action = $this->generateUrl('filrouge_user_update', array('id' => $id));
+
+           // return $this->handleForm($user, $action, $req);  
+            
+            return new Response("Bientôt, il y aura un super formulaire ici :) ");
+        }
     
-    public function removeAction($id, Request $req) {
-        return new Response('Suppression utilisateur' . $id);
+    public function removeAction($id, Request $req) 
+        {
+            $user = $this->getDoctrine()
+                         ->getRepository('AppBundle:User')
+                         ->findOneUserEager($id);
+               
+            $em = $this->getDoctrine()
+                       ->getManager();
+            
+            $em->remove($user);
+           
+            if ($user->getImage() !== null)
+                {
+                    $em->remove($user->getImage());
+                }
+            $em->flush();
+            
+            $url = $this->generateUrl('filrouge_user_list');
+                            
+            return $this->redirect($url);
     }
 }
