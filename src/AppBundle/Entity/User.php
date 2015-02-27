2a74c9486dcda94a01067b236259cfa9f008be3c
benjamin.dl10 <benjamin.dl10@PC-NA-20.formation.imie>
2015-02-27 10:33:09 +0100

    Merge origin/master
    Conflicts:
    	src/AppBundle/Resources/config/routing.yml

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -42,7 +42,7 @@ class User implements UserInterface
     /**
      * @var string
      *
-     * @ORM\Column(name="username", type="string", length=255)
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
      */
     private $username;
 
